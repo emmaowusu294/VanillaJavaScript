d02c8638cdb28109cf6d7d96185aa1cae9bbc81a
4Ts-code <eo8407272@gmail.com>
2025-03-12 01:02:04 +0000

    Variables

--- Basics/Variables/app.js
+++ Basics/Variables/app.js
@@ -1,3 +1,19 @@
-let var = 'variable';
+let Emma = 'variable';
 
-console.log(var);
+// const Emma = 'variable';
+
+Emma = 'A boy';
+
+console.log(Emma);
+
+// Output: A boy
+
+
+let name = 'Emmanuel Owusu';
+let whatDoYouWantToDoInLife = 'programmer';
+let gender = 'male';
+let twitterHandle = 'https://twitter.com/eowusu294';
+
+console.log(`My name is ${name}. I am a ${gender}. I want 
+    to be a ${whatDoYouWantToDoInLife}. My Twitter handle 
+    is ${twitterHandle}`);
