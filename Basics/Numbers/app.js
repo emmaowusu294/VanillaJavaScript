445df23e0e33ace46740f41183f44be44ac579ba
4Ts-code <eo8407272@gmail.com>
2025-03-12 01:13:01 +0000

    Numbers is a simple

---
+++ Basics/Numbers/app.js
@@ -0,0 +1,22 @@
+let num = 2;
+
+
+// console.log(num);
+
+// console.log(typeof num);
+
+
+// Basic Math
+
+// console.log(2 % 2);
+
+// console.log(Math.sqrt(25));
+
+// console.log(Math.pow(2, 5));
+
+
+let counter = 0;
+counter -= 1;
+counter += 1;
+
+console.log(counter);
