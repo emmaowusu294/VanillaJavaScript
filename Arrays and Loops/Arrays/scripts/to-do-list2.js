64451da528250a8af984b56d4f852254077fe107
4Ts-code <eo8407272@gmail.com>
2025-04-19 21:46:31 +0000

    improving code with addEventListener

--- Arrays and Loops/Arrays/scripts/to-do-list2.js
+++ Arrays and Loops/Arrays/scripts/to-do-list2.js
@@ -188,6 +188,12 @@ const todoList = JSON.parse(localStorage.getItem('activeTodoList')) || [];
 
 renderTodoList();
 
+
+document.querySelector('.js-add-todo-button')
+    .addEventListener('click', () => {
+        addTodo();
+    });
+
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
@@ -223,9 +229,7 @@ function renderTodoList() {
         const html =
             `<div>${name}</div>
              <div>${dueDateTime}</div>
-             <button onclick = "
-            deleteTodo(${index});
-             "class = "delete-todo-button">Delete</button>
+             <button class = "delete-todo-button js-delete-todo-button">Delete</button>
             `;
         todoListHTML += html;
     });
@@ -252,8 +256,17 @@ function renderTodoList() {
     document.querySelector('.js-todo-list')
         .innerHTML = todoListHTML;
 
-}
 
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(index, 1);
+                renderTodoList();
+            })
+        })
+
+}
+ 
 
 //Allowing enter key press to add the todo list to the list
 function addTodoKeydown(event) {
