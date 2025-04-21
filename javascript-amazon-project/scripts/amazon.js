144277fde81deadc1f9ccd4b4b8956edf0760b21
4Ts-code <eo8407272@gmail.com>
2025-04-21 01:43:59 +0000

    Move addToCart into cart.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js'
 
 let productsHTML = ``;
@@ -60,53 +60,38 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
     .innerHTML = productsHTML;
 
+function updateCartQuantity() {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    })
+
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+};
+
+function showAdded() {
+    
+    const addedMessages = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedMessages.classList.add('added-visible'); // ✅ Use quotes for the class name
+
+    
+    
+    setTimeout(() => {
+        addedMessages.classList.remove('added-visible');
+    }, 2000)
+};
 
    
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click',() => {
             const productId = button.dataset.productId;
-
-            let matchingItem;
-            cart.forEach((item) => {
-                if (productId === item.productId) {
-                    matchingItem = item;
-                }
-
-            });
-
-            if (matchingItem) {
-                matchingItem.quantity++;
-            } else {
-                cart.push({
-                    productId : productId,
-                    quantity: 1
-                });
-            }
-
-            let cartQuantity = 0;
-           cart.forEach((item) => {
-                cartQuantity += item.quantity;
-           })
-        
-
-        document.querySelector('.js-cart-quantity')
-            .innerHTML = cartQuantity;
-
-        const addedMessages = document.querySelector(`.js-added-to-cart-${productId}`);
-        addedMessages.classList.add('added-visible'); // ✅ Use quotes for the class name
-
-        
-        
-        setTimeout(() => {
-            addedMessages.classList.remove('added-visible');
-        }, 2000)
-
-       
-            
-          
+            addToCart(productId);
+            updateCartQuantity();
+            showAdded();
+          });
         });
-        
-    });
     
 
