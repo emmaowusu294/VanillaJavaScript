cce8fb5ff0587ebd8593a4831f46029fb93b3ef8
4Ts-code <eo8407272@gmail.com>
2025-04-24 03:57:25 +0000

    create oop version carts

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkoutt/orderSummary.js";
 import { renderPaymentSummary } from "./checkoutt/paymentSummary.js";
+import '../data/cart-oop.js'
 
 renderOrderSummary();
 renderPaymentSummary();
